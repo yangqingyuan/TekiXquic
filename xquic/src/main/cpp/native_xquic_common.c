1216b41a3daa7dde8100456d7d0d0b0d39bba45d
yangqingyuan <yangqingyuan@lizhi.fm>
2022-06-02 15:33:08 +0800

    # fix bug

--- xquic/src/main/cpp/native_xquic_common.c
+++ xquic/src/main/cpp/native_xquic_common.c
@@ -5,6 +5,60 @@
 static JavaVM *g_jvm;
 static pthread_mutex_t mutex;
 
+
+void correctUtfBytes(char* bytes) {
+    char three = 0;
+    while (*bytes != '\0') {
+        unsigned char utf8 = *(bytes++);
+        three = 0;
+        // Switch on the high four bits.
+        switch (utf8 >> 4) {
+            case 0x00:
+            case 0x01:
+            case 0x02:
+            case 0x03:
+            case 0x04:
+            case 0x05:
+            case 0x06:
+            case 0x07:
+                // Bit pattern 0xxx. No need for any extra bytes.
+                break;
+            case 0x08:
+            case 0x09:
+            case 0x0a:
+            case 0x0b:
+            case 0x0f:
+                /*
+                 * Bit pattern 10xx or 1111, which are illegal start bytes.
+                 * Note: 1111 is valid for normal UTF-8, but not the
+                 * modified UTF-8 used here.
+                 */
+                *(bytes-1) = '?';
+                break;
+            case 0x0e:
+                // Bit pattern 1110, so there are two additional bytes.
+                utf8 = *(bytes++);
+                if ((utf8 & 0xc0) != 0x80) {
+                    --bytes;
+                    *(bytes-1) = '?';
+                    break;
+                }
+                three = 1;
+                // Fall through to take care of the final byte.
+            case 0x0c:
+            case 0x0d:
+                // Bit pattern 110x, so there is one additional byte.
+                utf8 = *(bytes++);
+                if ((utf8 & 0xc0) != 0x80) {
+                    --bytes;
+                    if(three)--bytes;
+                    *(bytes-1)='?';
+                }
+                break;
+        }
+    }
+}
+
 /**
  * callback msg to java
  * @param object_android
@@ -31,6 +85,7 @@ void callback_msg_to_java(void *object_android, MSG_TYPE msg_type, const char *d
     /* data to jstring*/
     jstring recv_body;
     if (data != NULL) {
+        correctUtfBytes(data);
         recv_body = (*env)->NewStringUTF(env, data);
     } else {
         recv_body = (*env)->NewStringUTF(env, "");
